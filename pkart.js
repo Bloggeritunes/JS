0404eee3357dcfad387e33ea8efef70558b2af49
Bloggeritunes <bloggeritunes@gmail.com>
2016-10-14 15:22:39 +0300

    Add files via upload

---
+++ pkart.js
@@ -0,0 +1,13 @@
+﻿/*global $, console*/
+
+$(function () {
+   
+    'use strict';
+    
+    $('.pKart_add').click(function () {
+        $('.pKart_kart').toggleClass('pKart_on');
+        $('.pKart_add i').toggleClass('fa-minus');
+    });
+
+});
+/* Blogger Eklentileri | http://bloggereklentileri.blogspot.com.tr/ */
